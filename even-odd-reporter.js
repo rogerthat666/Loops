ca761cab68f253d1855f116c9745e6c624938cee
Rogier van Oudheusden <rogerthat@live.nl>
2021-12-17 15:53:02 +0100

    Add files via upload

---
+++ even-odd-reporter.js
@@ -0,0 +1,10 @@
+for (let i = 0; i <= 20; i++) {
+    console.log(`Is ${i} even or odd?`);
+    const oddeven = (i % 2);
+    if (oddeven == 1) {
+    console.log( i, " is odd");}
+    else if (oddeven == 0)
+    console.log( i, " is even");
+}
+
+
---
+++ multiplication-tables.js
@@ -0,0 +1,26 @@
+for (let i = 1; i <= 10; i++) {
+const tableOfOne = (i*1)
+const tableOfTwo = (i*2)
+const tableOfThree = (i*3)
+const tableOfFour = (i*4)
+const tableOfFive = (i*5)
+const tableOfSix = (i*6)
+const tableOfSeven = (i*7)
+const tableOfEight = (i*8)
+const tableOfNine = (i*9)
+const tableOfTen = (i*10)
+console.log(`${i} X 1 =`, tableOfOne);
+console.log(`${i} X 2 =`, tableOfTwo);
+console.log(`${i} X 3 =`, tableOfThree);
+console.log(`${i} X 4 =`, tableOfFour);
+console.log(`${i} X 5 =`, tableOfFive);
+console.log(`${i} X 6 =`, tableOfSix);
+console.log(`${i} X 7 =`, tableOfSeven);
+console.log(`${i} X 8 =`, tableOfEight);
+console.log(`${i} X 9 =`, tableOfNine);
+console.log(`${i} X 10 =`, tableOfTen);
+if (i === 10) {
+    return i;
+}
+}
+
